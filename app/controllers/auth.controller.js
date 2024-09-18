35c53d328756f1d00f1cad38579ff45a8af43280
dimitrisCBR <dimitsigou@gmail.com>
2024-09-18 22:40:51 +0300

    users registered via public API will always be role user

--- app/controllers/auth.controller.js
+++ app/controllers/auth.controller.js
@@ -17,22 +17,9 @@ exports.signup = async (req, res) => {
       password: bcrypt.hashSync(req.body.password, 8),
     });
 
-    if (req.body.roles) {
-      const roles = await Role.findAll({
-        where: {
-          name: {
-            [Op.or]: req.body.roles,
-          },
-        },
-      });
-
-      const result = user.setRoles(roles);
-      if (result) res.send({ message: "User registered successfully!" });
-    } else {
-      // user has role = 1
-      const result = user.setRoles([1]);
-      if (result) res.send({ message: "User registered successfully!" });
-    }
+    // user has role = 1
+    const result = user.setRoles([1]);
+    if (result) res.send({ message: "User registered successfully!" });
   } catch (error) {
     res.status(500).send({ message: error.message });
   }
@@ -61,13 +48,11 @@ exports.signin = async (req, res) => {
       });
     }
 
-    const token = jwt.sign({ id: user.id },
-                           config.secret,
-                           {
-                            algorithm: 'HS256',
-                            allowInsecureKeySizes: true,
-                            expiresIn: 86400, // 24 hours
-                           });
+    const token = jwt.sign({ id: user.id }, config.secret, {
+      algorithm: config.hasing_algorithm,
+      allowInsecureKeySizes: true,
+      expiresIn: config.jwt_expires_in,
+    });
 
     let authorities = [];
     const roles = await user.getRoles();
@@ -82,6 +67,7 @@ exports.signin = async (req, res) => {
       username: user.username,
       email: user.email,
       roles: authorities,
+      jwt : token
     });
   } catch (error) {
     return res.status(500).send({ message: error.message });
@@ -92,7 +78,7 @@ exports.signout = async (req, res) => {
   try {
     req.session = null;
     return res.status(200).send({
-      message: "You've been signed out!"
+      message: "You've been signed out!",
     });
   } catch (err) {
     this.next(err);
